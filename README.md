a0175f5c3a64d95e765db588f146db5df3d010e0
DarksomeV <viktor.khyshnyak@gmail.com>
2019-11-09 16:36:37 +0200

    step 5 ready

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage)](https://codeclimate.com/github/codeclimate/codeclimate/test_coverage)
 [![Build Status](https://travis-ci.org/DarksomeV/frontend-project-lvl1.svg?branch=master)](https://travis-ci.org/DarksomeV/frontend-project-lvl1)
+
+
+##### brain-even
+Brain-even give a number and you must type is it even or not. Answers have to be "yes" or "not", other answers are error.
+
+### Installation
+sudo npm install -g hexlet_brain_games
+
+### Run
+Each game runs with it name.
--- src/index.js
+++ src/index.js
@@ -1,10 +1,57 @@
 import readlineSync from 'readline-sync';
 
-const startWelcomeGame = () => {
+const startWelcomeGame = (gameDescription) => {
   console.log('Welcome to the Brain Games!');
-  console.log('');
+  console.log(gameDescription || '');
 
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
+
+export const initLogic = (getGameData, user) => {
+  const iter = (acc) => {
+    const gameData = getGameData();
+    console.log(`Question:${gameData.question}`);
+    const answer = readlineSync.question('Your answer: ');
+    const rightAnswer = gameData.answer;
+    if (answer === rightAnswer && acc === 2) {
+      console.log(`Congratulations, ${user}!`);
+      return true;
+    } if (answer === rightAnswer) {
+      console.log('Correct!');
+      return iter((acc + 1));
+    }
+
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+    console.log(`Let's try again, ${user}!`);
+    return false;
+  };
+
+  return iter(0);
+};
+
+export const welcomeEvenGame = () => {
+  const gameDescr = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const user = startWelcomeGame(gameDescr);
+
+  const isEven = (n) => n % 2 === 0;
+
+  const getRightAnswer = (n) => (isEven(n) ? 'yes' : 'no');
+
+  const getRoundRandomNumber = (min, max) => Math.round((Math.random() * (max - min)) + min);
+
+  const getGameData = () => {
+    const question = getRoundRandomNumber(1, 100);
+    const answer = getRightAnswer(question);
+    return {
+      question,
+      answer,
+    };
+  };
+
+  initLogic(getGameData, user);
+};
+
+
 export default startWelcomeGame;
